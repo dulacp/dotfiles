7c4fca47afa54cce3ba6b9e9343e20e383051e68
Pierre Dulac <dulacpier@gmail.com>
2025-04-18 11:36:20 +0200

    Add autocompletion for zshrc

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Kubernetes
+source <(kubectl completion zsh)
+
+# Scaleway CLI autocomplete initialization.
+eval "$(scw autocomplete script shell=zsh)"
